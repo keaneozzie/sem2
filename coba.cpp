edac901274f60e67c802ba2e242796c095234207
keaneozzie <ozziedharma897@gmail.com>
2026-02-23 17:16:58 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,12 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ //program utama
+    int age;  //variable local
+    int age1;  //variable local
+    age = 20;
+    age1 = 15;
