56ee398a3935d4ce0c94a3ef9b8db0f3bb2b9403
keaneozzie <ozziedharma897@gmail.com>
2026-03-02 16:54:44 +0700

    menambahkan float phi

---
+++ challenge.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using  namespace std;
+
+float phi = 3.14159;
+      int r;
+
+
+
+void input(){
+    cout << "Masukan Luas: ";
+    cin >> r;
+}
+
+int luaslingkaran(int r){
+    return r;
+}
+int jumlah(int phi, int r, int luas){
+    return phi * r * r + luas;
+}
+
+void output(){
+    cout << "Hasilnya : " <<jumlah(phi, r, luaslingkaran(r)) << endl;
+}
+int main(){
+    input();
+    output();
+    cout << "Hasilnya: " <<luaslingkaran(r) << endl;
+} 
